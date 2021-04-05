b9be2b4e381eaa957013f8d78e26f2bb1c6a3dce
Gushihiro <robinsonhiroto@gmail.com>
2021-04-05 12:44:48 -0700

    creating memes array

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
-// test statement
-console.log("js linked");
+var memeArray = [
+    
+]
+var randomIndex = Math.floor(Math.Random)
+function getApi(event) {
+      // fetch request gets a list of all the repos for the node.js organization  
+    var requestUrl = 'https://api.imgflip.com/get_memes';
+    console.log(requestUrl)
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {     
+          console.log(data)
+          console.log(data.data.memes[0].url)
+          console.log(data.data.memes[0].)
+    })
+}
+getApi();
