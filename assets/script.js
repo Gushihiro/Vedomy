3f996342d98a84582a0ca2f09b0760746b498d00
Gushihiro <robinsonhiroto@gmail.com>
2021-04-05 20:09:04 -0700

    working on inspirational quotes

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,4 @@
-var memeArray = [];
-
-var randomIndex = Math.floor(Math.Random);
-function getApi(event) {
-  // fetch request gets a list of all the repos for the node.js organization  
-  var requestUrl = 'https://api.imgflip.com/get_memes';
-  console.log(requestUrl);
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {     
-        console.log(data);
-        console.log(data.data.memes[0].url);
-        console.log(data.data.memes[0]);
-    })
-}
-getApi();
-
-
-// fetches JSON data from News API
-// API key: 72274a0b422f439fb0c2d607f98ef1ad
-function checkNewsApi () {
-  var NewsUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=72274a0b422f439fb0c2d607f98ef1ad';
-
-  fetch(NewsUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {     
-    console.log(data);
-  })
-}
-
-checkNewsApi();
-
+//cloudinary
 // page link test
 console.log("js linked");
 
@@ -41,3 +6,15 @@ console.log("js linked");
 var timeCheck = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 console.log("Current date and time is " + timeCheck);
 
+
+function getApi() {
+    var zenQuote = 'https://type.fit/api/quotes';
+    fetch(zenQuote)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {     
+        console.log(data)
+  })
+};
+getApi();
